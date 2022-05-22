8039ca5cb1b44a57c3ecea3a8d322fa18a148dfe
Mohamed Yassin <95443053+Gemy-Creatio@users.noreply.github.com>
2022-05-22 22:26:35 +0200

    finish home-page

--- main/views/home_page.py
+++ main/views/home_page.py
@@ -2,16 +2,23 @@ from django.views import View
 from django.shortcuts import render
 from states.models import (
     Developers,
-    Department
+    Department,
+    Projects,
 )
+from ..models import Blog
 
 
 class HomePageView(View):
     def get(self, request):
         developers = Developers.objects.all().order_by('-pk')[:4]
         departments = Department.objects.all().order_by('-pk')[:6]
+        projects = Projects.objects.all().order_by('-pk')[:12]
+        blogs = Blog.objects.all().order_by('-pk')[:4]
+
         context = {
             "developers": developers,
-            "departments": departments
+            "departments": departments,
+            "projects": projects,
+            "blogs": blogs
         }
         return render(request, 'main/homepage/homepage.html', context=context)
--- states/models/projects_model.py
+++ states/models/projects_model.py
@@ -1,18 +1,52 @@
 from django.db import models
 from ..models import Department
+from django.templatetags.static import static
 
 
 class Projects(models.Model):
     project_name = models.CharField(max_length=255, null=True, blank=True)
     project_description = models.TextField(null=True, blank=True)
-    area = models.SmallIntegerField(null=True, blank=True)
+    area = models.CharField(max_length=255, null=True, blank=True)
     min_price = models.SmallIntegerField(null=True, blank=True)
     max_price = models.SmallIntegerField(null=True, blank=True)
     department = models.ForeignKey(Department, null=True, blank=True, related_name='projects', on_delete=models.CASCADE)
+    cover = models.ImageField(upload_to='projects-photos/', null=True, blank=True)
+    background_1 = models.ImageField(upload_to='projects-photos/', null=True, blank=True)
+    background_2 = models.ImageField(upload_to='projects-photos/', null=True, blank=True)
+    background_3 = models.ImageField(upload_to='projects-photos/', null=True, blank=True)
+    video_url = models.URLField(null=True, blank=True)
+
+    @property
+    def cover_url(self):
+        if self.cover and hasattr(self.cover, 'url'):
+            return self.cover.url
+        else:
+            return static("assets/img/gallery/cinnamon.png")
+
+    @property
+    def background_1_url(self):
+        if self.background_1 and hasattr(self.background_1, 'url'):
+            return self.background_1.url
+        else:
+            return static("assets/img/gallery/cinnamon.png")
+
+    @property
+    def background_2_url(self):
+        if self.background_1 and hasattr(self.background_2, 'url'):
+            return self.background_2.url
+        else:
+            return static("assets/img/gallery/cinnamon.png")
+
+    @property
+    def background_3_url(self):
+        if self.background_1 and hasattr(self.background_3, 'url'):
+            return self.background_3.url
+        else:
+            return static("assets/img/gallery/cinnamon.png")
 
     def __str__(self) -> str:
         return f"Name : {self.project_name}"
 
     @property
-    def carsoualNbr(self):
-        return self.pk * 100
+    def CheckEvenPK(self):
+        return True if (self.pk % 2) == 0 else False
